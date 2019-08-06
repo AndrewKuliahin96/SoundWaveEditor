2cfcf0af63023d84771191fdfd81e5fbf88c0030
Andrey Kuliagin <andrey.kuliagin.cr@gmail.com>
2019-08-06 18:14:57 +0300

    add some of gestures

--- app/src/main/java/com/example/soundwaveeditor/MainActivity.kt
+++ app/src/main/java/com/example/soundwaveeditor/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.soundwaveeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 import kotlin.random.nextUBytes
@@ -11,46 +10,23 @@ import kotlin.random.nextUBytes
 @ExperimentalUnsignedTypes
 class MainActivity : AppCompatActivity() {
 
-    private val onSeekBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
-
-        override fun onProgressChanged(seekBar: SeekBar, p1: Int, p2: Boolean) {
-            val pos = vSoundEditor.currentColumnsCount / 100 * p1
-
-            when (seekBar.id) {
-                R.id.sbLeft -> { vSoundEditor.leftSlideBar = pos }
-                R.id.sbRight -> { vSoundEditor.rightSlideBar = pos }
-                R.id.sbFirstPosition -> {
-                    vSoundEditor.firstVisibleColumn = (vSoundEditor.columnBytes.size  - vSoundEditor.currentColumnsCount) / 100 * p1
-                }
-            }
-        }
-
-        override fun onStartTrackingTouch(p0: SeekBar?) = Unit
-
-        override fun onStopTrackingTouch(p0: SeekBar?) = Unit
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawHistogram()
-
-        sbLeft.setOnSeekBarChangeListener(onSeekBarChangeListener)
-        sbRight.setOnSeekBarChangeListener(onSeekBarChangeListener)
-        sbFirstPosition.setOnSeekBarChangeListener(onSeekBarChangeListener)
     }
 
     private fun drawHistogram() {
-        val soundDurationMs = 65_000
+        val soundDurationMs = 265_000
 
         val bytes = getUBytes(1_900).toMutableList()
 
         vSoundEditor.columnBytes = bytes
         vSoundEditor.maxColumnsCount = 800
-        vSoundEditor.currentColumnsCount = 700
-        vSoundEditor.firstVisibleColumn = 200
-        vSoundEditor.rightSlideBar = 400
-        vSoundEditor.leftSlideBar = 100
+        vSoundEditor.currentColumnsCount = 200
+        vSoundEditor.firstVisibleColumn = 20
+        vSoundEditor.rightSlideBar = 150
+        vSoundEditor.leftSlideBar = 50
         vSoundEditor.soundDuration = soundDurationMs
     }
 
--- app/src/main/java/com/example/soundwaveeditor/view/SoundWaveEditorView.kt
+++ app/src/main/java/com/example/soundwaveeditor/view/SoundWaveEditorView.kt
@@ -1,29 +1,39 @@
 package com.example.soundwaveeditor.view
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.view.GestureDetectorCompat
 import com.example.soundwaveeditor.R
+import kotlin.math.absoluteValue
 
 
 @ExperimentalUnsignedTypes
-class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context, attrs),
+    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
     companion object {
+        private const val MINUTES_TWO_SYMBOLS_TIME_FORMAT = "%02d:%02d"
+        private const val MINUTES_ONE_SYMBOL_TIME_FORMAT = "%01d:%02d"
+        private const val DEFAULT_VERTICAL_PADDING_RATIO = 0.25F
+        private const val DEFAULT_HISTOGRAM_TOP_PADDING = 0.1F
+        private const val DEFAULT_TIME_TEXT_SIZE = 14F
+        private const val DEFAULT_COLUMNS_RATIO = 1F
+        private const val ZERO_SIZE_F = 0F
+        private const val ZERO_SIZE = 0
         private const val DEFAULT_SLIDE_BARS_PADDING = 10
         private const val DEFAULT_MAX_COLUMNS_COUNT = 100
         private const val DEFAULT_MIN_COLUMNS_COUNT = 20
         private const val DEFAULT_COLUMNS_COUNT = 50
-        private const val DEFAULT_TIME_TEXT_SIZE = 14F
-        private const val DEFAULT_HISTOGRAM_TOP_PADDING = 0.1F
-        private const val DEFAULT_VERTICAL_PADDING_RATIO = 0.25F
-        private const val DEFAULT_COLUMNS_RATIO = 1F
-        private const val DEFAULT_ZERO_SIZE = 0F
         private const val ZERO_SOUND_DURATION = 0
-        private const val MINUTES_TWO_SYMBOLS_TIME_FORMAT = "%02d:%02d"
-        private const val MINUTES_ONE_SYMBOL_TIME_FORMAT = "%01d:%02d"
     }
+
+    private var gestureDetector: GestureDetectorCompat
 
     private val histogramTopPaddingRatioRange = 0F..2F
     private val verticalPaddingRatioRange = 0F..0.5F
@@ -33,13 +43,13 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
     private var halfOfHistogramTopPadding = DEFAULT_HISTOGRAM_TOP_PADDING
     private var spacingBetweenColumns = DEFAULT_COLUMNS_RATIO
     private var columnWidth = DEFAULT_COLUMNS_RATIO
-    private var columnRadius = DEFAULT_ZERO_SIZE
+    private var columnRadius = ZERO_SIZE_F
 
-    private var fWidth = DEFAULT_ZERO_SIZE
-    private var fHeight = DEFAULT_ZERO_SIZE
+    private var fWidth = ZERO_SIZE_F
+    private var fHeight = ZERO_SIZE_F
 
-    private var histogramXAxis = DEFAULT_ZERO_SIZE
-    private var histogramHeight = DEFAULT_ZERO_SIZE
+    private var histogramYAxis = ZERO_SIZE_F
+    private var histogramHeight = ZERO_SIZE_F
 
     private var histogramBackgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -62,8 +72,13 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
         textSize = DEFAULT_TIME_TEXT_SIZE
     }
 
-    private var histogramBackgroundRectF = RectF(0F, 0F, 0F, 0F)
-    private val textRect = Rect(0, 0, 0, 0)
+    private var histogramBackgroundRectF = RectF(ZERO_SIZE_F, ZERO_SIZE_F, ZERO_SIZE_F, ZERO_SIZE_F)
+
+    private val drawingRectF = RectF(ZERO_SIZE_F, ZERO_SIZE_F, ZERO_SIZE_F, ZERO_SIZE_F)
+
+    private val textRect = Rect(ZERO_SIZE, ZERO_SIZE, ZERO_SIZE, ZERO_SIZE)
+
+    private val columns = mutableListOf<ColumnSize>()
 
     var timeTextSize = DEFAULT_TIME_TEXT_SIZE
         set(value) = field.getIfNewAndInvalidate(value) {
@@ -161,18 +176,16 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
             field = it
         }
 
-    var columns = mutableListOf<Float>()
-
     var leftSlideBar = DEFAULT_SLIDE_BARS_PADDING
         set(value) = field.getIfAndInvalidate(value, {
-            value in DEFAULT_SLIDE_BARS_PADDING until rightSlideBar - slideBarsPaddingInColumns
+            value in DEFAULT_SLIDE_BARS_PADDING..rightSlideBar - slideBarsPaddingInColumns
         }) {
             field = it
         }
 
     var rightSlideBar = DEFAULT_COLUMNS_COUNT - DEFAULT_SLIDE_BARS_PADDING
         set(value) = field.getIfAndInvalidate(value, {
-            value in leftSlideBar + slideBarsPaddingInColumns..currentColumnsCount - DEFAULT_SLIDE_BARS_PADDING
+            value in leftSlideBar + slideBarsPaddingInColumns - 1 until currentColumnsCount - DEFAULT_SLIDE_BARS_PADDING
         }) {
             field = when (it == currentColumnsCount) {
                 true -> it - 1
@@ -183,8 +196,7 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.SoundWaveEditorView, 0, 0).apply {
             timeTextSize = getDimension(
-                R.styleable.SoundWaveEditorView_timeTextSize,
-                DEFAULT_TIME_TEXT_SIZE
+                R.styleable.SoundWaveEditorView_timeTextSize, DEFAULT_TIME_TEXT_SIZE
             )
 
             timeTextColor = getColor(
@@ -213,18 +225,15 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
             )
 
             maxColumnsCount = getInt(
-                R.styleable.SoundWaveEditorView_maxColumnsCount,
-                DEFAULT_MAX_COLUMNS_COUNT
+                R.styleable.SoundWaveEditorView_maxColumnsCount, DEFAULT_MAX_COLUMNS_COUNT
             )
 
             minColumnsCount = getInt(
-                R.styleable.SoundWaveEditorView_minColumnsCount,
-                DEFAULT_MIN_COLUMNS_COUNT
+                R.styleable.SoundWaveEditorView_minColumnsCount, DEFAULT_MIN_COLUMNS_COUNT
             )
 
             currentColumnsCount = getInt(
-                R.styleable.SoundWaveEditorView_currentColumnsCount,
-                DEFAULT_COLUMNS_COUNT
+                R.styleable.SoundWaveEditorView_currentColumnsCount, DEFAULT_COLUMNS_COUNT
             )
 
             slideBarsPaddingInColumns = getInt(
@@ -249,6 +258,8 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
                 )
         }.recycle()
 
+        setLayerType(LAYER_TYPE_HARDWARE, null)
+
         histogramBackgroundPaint.color = histogramBackgroundColor
         activeColumnsPaint.color = activeColumnsColor
         inactiveColumnsPaint.color = inactiveColumnsColor
@@ -257,16 +268,13 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
             color = timeTextColor
             textSize = timeTextSize
         }
+
+        gestureDetector = GestureDetectorCompat(context, this)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-
-        fWidth = w.toFloat()
-        fHeight = h.toFloat()
-
-        getElementsSizes()
-        getColumnsHeight()
+        getElementsSizes(w, h)
     }
 
     override fun onDraw(canvas: Canvas) = canvas.run {
@@ -276,7 +284,7 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
 
         columns.takeIf { it.size >= lastVisibleItem }
             ?.subList(firstVisibleColumn, lastVisibleItem)
-            ?.forEachIndexed { index, halfColumnHeight ->
+            ?.forEachIndexed { index, column ->
                 var isSlideBar = false
 
                 when (index) {
@@ -284,11 +292,18 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
                     leftSlideBar, rightSlideBar -> slideBarPaint.apply { isSlideBar = true }
                     else -> inactiveColumnsPaint
                 }.let { paint ->
-                    val rectF = getRectF(index, halfColumnHeight, isSlideBar)
+                    val leftColumnSide = index * columnWidth + index * spacingBetweenColumns
+
+                    drawingRectF.run {
+                        left = leftColumnSide
+                        right = leftColumnSide + columnWidth
+                        top = if (isSlideBar) halfOfHistogramTopPadding else column.top
+                        bottom = if (isSlideBar) fHeight else column.bottom
+                    }
 
                     when (needToRoundColumns) {
-                        true -> drawRoundRect(rectF, columnRadius, columnRadius, paint)
-                        false -> drawRect(rectF, paint)
+                        true -> drawRoundRect(drawingRectF, columnRadius, columnRadius, paint)
+                        false -> drawRect(drawingRectF, paint)
                     }
                 }
             }
@@ -297,49 +312,122 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
         getTimeAndPosition(rightSlideBar) { text, x, y -> drawText(text, x, y, timeTextPaint) }
     }
 
-    private fun getRectF(
-        position: Int,
-        halfColumnHeight: Float,
-        isSlideBar: Boolean = false
-    ): RectF {
-        val left = position * columnWidth + position * spacingBetweenColumns
-        val right = left + columnWidth
 
-        return when (isSlideBar) {
-            true -> RectF(left, halfOfHistogramTopPadding, right, fHeight)
-            false -> RectF(
-                left,
-                histogramXAxis - halfColumnHeight,
-                right,
-                histogramXAxis + halfColumnHeight
-            )
-        }
+    // TODO rename all, remove unused
+
+
+    // TODO remove later this shit nakhui
+
+    override fun onTouchEvent(event: MotionEvent) =
+        gestureDetector.onTouchEvent(event).takeIf { it }?.let { it } ?: super.onTouchEvent(event)
+
+
+    // TODO double taps
+    override fun onDoubleTap(p0: MotionEvent?): Boolean {
+        Log.e("GESTURES", "onDoubleTap")
+
+        return true
     }
+
+    override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
+        Log.e("GESTURES", "onDoubleTapEvent")
+
+        return true
+    }
+
+    override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
+        Log.e("GESTURES", "onSingleTapConfirmed")
+
+        return true
+    }
+
+
+
+    override fun onShowPress(p0: MotionEvent?) {
+        Log.e("GESTURES", "onShowPress")
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        Log.e("GESTURES", "onSingleTapUp")
+
+        valueAnimator?.takeIf { it.isRunning }?.cancel()
+
+        return true
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        Log.e("GESTURES", "onDown")
+
+        return true
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        Log.e("GESTURES", "onFling $p2")
+
+        valueAnimator?.takeIf { it.isRunning }?.apply {
+            val oldFlingEnd = finishValue == 0
+            val newFlingEnd = p2 > 0
+
+            if ((oldFlingEnd && newFlingEnd) || (!oldFlingEnd && !newFlingEnd)) {
+                duration /= 2
+            } else {
+                cancel()
+            }
+        } ?: startOnFlingAnimation(p2)
+
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        Log.e("GESTURES", "onScroll, $p2")
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        Log.e("GESTURES", "onLongPress")
+    }
+
+    private var valueAnimator: ValueAnimator? = null
+    private var finishValue: Int = 0
+
+    private fun startOnFlingAnimation(flingPower: Float) {
+        if (flingPower.absoluteValue > 7_000F) {
+            finishValue = flingPower.takeIf { it > 0 }?.let { 0 } ?: let {
+                columnBytes.size - currentColumnsCount
+            }
+
+            valueAnimator = ValueAnimator.ofInt(firstVisibleColumn, finishValue).apply {
+                // TODO change this value to ratio with volume columns
+                duration = 2_500L
+                addUpdateListener { animation -> firstVisibleColumn = (animation.animatedValue as Int) }
+                start()
+            }
+
+        } else return
+    }
+
+
+
+
 
     private fun getTimeAndPosition(position: Int, result: (String, Float, Float) -> Unit) {
         columnBytes.takeIf { it.isNotEmpty() }?.let {
             val timeText = getTimeText(position + firstVisibleColumn)
 
             timeTextPaint.run {
-                getTextBounds(timeText, 0, timeText.length, textRect)
+                getTextBounds(timeText, ZERO_SIZE, timeText.length, textRect)
 
                 var xPos =
                     position * columnWidth + position * spacingBetweenColumns + halfOfHistogramTopPadding
 
-                val rightSlideBarPos =
-                    rightSlideBar * columnWidth + rightSlideBar * spacingBetweenColumns
+                val rightSlideBarPos = rightSlideBar * (columnWidth + spacingBetweenColumns)
+                val textWidth = xPos + textRect.right.toFloat() * 2
 
-                when (position) {
-                    leftSlideBar -> {
-                        if (xPos + textRect.right.toFloat() >= rightSlideBarPos) {
-                            xPos -= textRect.right * 2 + halfOfHistogramTopPadding
-                        }
-                    }
-                    rightSlideBar -> {
-                        if (xPos + textRect.right.toFloat() >= fWidth) {
-                            xPos -= textRect.right * 2 + halfOfHistogramTopPadding
-                        }
-                    }
+                val isLeftSlideBar = position == leftSlideBar && textWidth >= rightSlideBarPos
+                val isRightSlideBar = position == rightSlideBar && textWidth >= fWidth
+
+                if (isLeftSlideBar || isRightSlideBar) {
+                    xPos -= textRect.right * 2 + halfOfHistogramTopPadding
                 }
 
                 result(timeText, xPos, (halfOfHistogramTopPadding - (descent() + ascent()) / 2F))
@@ -348,7 +436,7 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
     }
 
     private fun getTimeText(position: Int): String {
-        val millis = soundDuration / (columnBytes.size.takeIf { it != 0 } ?: 1) * position
+        val millis = soundDuration / (columnBytes.size.takeIf { it != ZERO_SIZE } ?: 1) * position
         val second = millis / 1_000 % 60
         val minute = millis / 60_000 % 60
 
@@ -356,7 +444,10 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
             ?: MINUTES_ONE_SYMBOL_TIME_FORMAT, minute, second)
     }
 
-    private fun getElementsSizes() {
+    private fun getElementsSizes(w: Int, h: Int) {
+        fWidth = w.toFloat()
+        fHeight = h.toFloat()
+
         histogramTopPadding = fHeight * histogramTopPaddingRatio
 
         columnWidth =
@@ -366,27 +457,29 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
 
         spacingBetweenColumns = columnWidth * columnSpacingRatio
 
-        histogramBackgroundRectF = RectF(DEFAULT_ZERO_SIZE, histogramTopPadding, fWidth, fHeight)
+        histogramBackgroundRectF = RectF(ZERO_SIZE_F, histogramTopPadding, fWidth, fHeight)
 
         halfOfHistogramTopPadding = histogramTopPadding / 2
 
         val heightWithTopPadding = fHeight - histogramTopPadding
 
-        histogramHeight =
-            heightWithTopPadding - heightWithTopPadding * columnVerticalPaddingRatio
+        histogramHeight = heightWithTopPadding - heightWithTopPadding * columnVerticalPaddingRatio
 
-        histogramXAxis =
+        histogramYAxis =
             histogramHeight / 2F + histogramTopPadding + fHeight * columnVerticalPaddingRatio / 2F
-    }
 
-    private fun getColumnsHeight() {
         columns.clear()
 
-        columnBytes.forEach { uByte ->
-            val columnHeight =
-                histogramHeight / UByte.MAX_VALUE.toFloat() * uByte.toFloat() - histogramTopPadding
+        val heightMin = histogramHeight / UByte.MAX_VALUE.toFloat()
 
-            columns.add(columnHeight / 2F)
+        columnBytes.forEach { uByte ->
+            val halfOfColumnHeight = (heightMin * uByte.toFloat() - histogramTopPadding) / 2F
+
+            columns.add(
+                ColumnSize(
+                    histogramYAxis - halfOfColumnHeight, histogramYAxis + halfOfColumnHeight
+                )
+            )
         }
     }
 
@@ -406,4 +499,6 @@ class SoundWaveEditorView(context: Context, attrs: AttributeSet) : View(context,
             invalidate()
         }
     } ?: Unit
+
+    data class ColumnSize(var top: Float, var bottom: Float)
 }
